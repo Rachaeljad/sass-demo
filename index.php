cddf9ee56e6116833d257cd89912f417f7e8d5c2
Rachaeljad <Rachaeljad@gmail.com>
2015-04-09 19:57:47 -0600

    sass-demo
    update mixins etc

--- index.php
+++ index.php
@@ -7,19 +7,19 @@ $pageTitle = 'Home';
 <?php include 'header.php';?>
 
 <div class="hero">
-	<div class="row clearfix">
+	<div class="row">
 	<h1>Hello <?php echo $userName; ?>, nice to see you again.</h1>
 	<?php if(empty($userName)) : ?>
-  <h1>Who are you? Login <a href="/login.php">here</a></h1>
+    <h1>Who are you? Login <a href="/login.php">here</a></h1>
 
-  <?php else : ?>
-  <h1><?php echo $userName; ?></h1>
-<?php endif; ?>
+ 	 <?php else : ?>
+    <h1><?php echo $userName; ?></h1>
+    <?php endif; ?>
 	</div>
 
 </div>
 
-<div class="row clearfix">
+<div class="row">
 	<div class="column one-half">
 		<h3> column heading</h3>
 		<p> Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
